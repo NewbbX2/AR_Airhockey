b106c5b15d08894ca61efa1b4e9df4f530930abf
Aqours <poweroeh@seoultech.ac.kr>
2019-09-06 11:01:54 +0900

    컨트롤러 재조정
    조정중

--- AR/StageController.cs
+++ AR/StageController.cs
@@ -1,91 +1,58 @@
 ﻿using System.Collections.Generic;
-using GoogleARCore;
-using GoogleARCore.Examples.Common;
+
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class StageController : MonoBehaviour
+using GoogleARCore;
+using GoogleARCore.Examples.Common;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class StageController : MonoBehaviourPunCallbacks, IPunObservable
 {
     public Camera FirstPersonCamera;
 
     #region AR Core 공간 감지 변수들
-    /// <summary>
-    /// A prefab for tracking and visualizing detected planes.
-    /// </summary>
-    public GameObject DetectedPlanePrefab;
-
-    /// <summary>
-    /// A model to place when a raycast from a user touch hits a vertical plane.
-    /// </summary>
-    public GameObject AndyVerticalPlanePrefab;
-
-    //수평면 감지에 설치될 것
-    /// <summary>
-    /// A model to place when a raycast from a user touch hits a horizontal plane.
-    /// </summary>
-    public GameObject AndyHorizontalPlanePrefab;
-
-    /// <summary>
-    /// A model to place when a raycast from a user touch hits a feature point.
-    /// </summary>
-    public GameObject AndyPointPrefab;
+    public GameObject DetectedPlanePrefab; // 감지된 평면을 시각화
+    public GameObject HockeyTablePrefab; // 실제로 배치 되는 테이블 프리팹
 
     //공간감지모드
-    private DetectedPlaneFindingMode AH_PlaneFindingMode = DetectedPlaneFindingMode.Horizontal;
+    private DetectedPlaneFindingMode AR_PlaneFindingMode;
     #endregion
 
-    /// <summary>
-    /// The rotation in degrees need to apply to model when the Andy model is placed.
-    /// </summary>
-    private const float k_ModelRotation = 180.0f;
+    //private const float ModelRotation = 180.0f;
 
-    /// <summary>
-    /// True if the app is in the process of quitting due to an ARCore connection error,
-    /// otherwise false.
-    /// </summary>
-    private bool m_IsQuitting = false;
 
-    /// <summary>
-    /// The Unity Awake() method.
-    /// </summary>
     public void Awake()
     {
-        // Enable ARCore to target 60fps camera capture frame rate on supported devices.
-        // Note, Application.targetFrameRate is ignored when QualitySettings.vSyncCount != 0.
-        Application.targetFrameRate = 60;
-        AH_PlaneFindingMode = DetectedPlaneFindingMode.Horizontal;//수평면만 감지하도록 설정
+        AR_PlaneFindingMode = DetectedPlaneFindingMode.Horizontal;//수평면만 감지하도록 설정
     }
 
-    /// <summary>
-    /// The Unity Update() method.
-    /// </summary>
+
     public void Update()
     {
-        _UpdateApplicationLifecycle();
-
-        // If the player has not touched the screen, we are done with this update.
+        // 터치 없으면 업데이트 종료
         Touch touch;
         if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
         {
             return;
         }
 
-        // Should not handle input if the player is pointing on UI.
+        // UI 터치하고 있으면 업데이트 종료
         if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
         {
             return;
         }
 
-        // Raycast against the location the player touched to search for planes.
+        // 평면 착기 위한 Raycast 설정
         TrackableHit hit;
         TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
             TrackableHitFlags.FeaturePointWithSurfaceNormal;
 
-        //Ray를 쏴서 감지된 평면에 닿으면 정해진 오브젝트 설치
         if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
         {
-            // Use hit pose and camera pose to check if hittest is from the
-            // back of the plane, if it is, no need to create the anchor.
+            //Ray가 부딪힌 장소와 카메라 포지션을 이용해서 생성된 평면 뒤가 아닌가 확인
             if ((hit.Trackable is DetectedPlane) &&
                 Vector3.Dot(FirstPersonCamera.transform.position - hit.Pose.position,
                     hit.Pose.rotation * Vector3.up) < 0)
@@ -94,118 +61,26 @@ public class StageController : MonoBehaviour
             }
             else
             {
-                // Choose the Andy model for the Trackable that got hit.
-                GameObject prefab;
-                if (hit.Trackable is FeaturePoint)
-                {
-                    prefab = AndyPointPrefab;
-                }
-                else if (hit.Trackable is DetectedPlane)
+
+                if (hit.Trackable is DetectedPlane)
                 {
                     DetectedPlane detectedPlane = hit.Trackable as DetectedPlane;
-                    if (detectedPlane.PlaneType == DetectedPlaneType.Vertical)
+                    //수평 윗면인지 확인해서 인스턴스 생성
+                    if (detectedPlane.PlaneType == DetectedPlaneType.HorizontalUpwardFacing)
                     {
-                        prefab = AndyVerticalPlanePrefab;
-                    }
-                    else
-                    {
-                        //Ray가 맞은 곳이 감지된 평면이고 그게 수평면이면 정해진것을 설치
-                        prefab = AndyHorizontalPlanePrefab;
+                        var gameTable = PhotonNetwork.Instantiate(HockeyTablePrefab.name, hit.Pose.position, hit.Pose.rotation);
+                        AR_PlaneFindingMode = DetectedPlaneFindingMode.Disabled; // AR 감지 끄기
+                        gameTable.transform.Rotate(0, 0, 0, Space.Self);
+                        var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+                        gameTable.transform.parent = anchor.transform;
                     }
                 }
-                else
-                {
-                    prefab = AndyHorizontalPlanePrefab;
-                }
-
-                var andyObject = Instantiate(prefab, hit.Pose.position, hit.Pose.rotation); // 설치Instantiate Andy model at the hit pose. 
-                AH_PlaneFindingMode = DetectedPlaneFindingMode.Disabled;
-
-                // Compensate for the hitPose rotation facing away from the raycast (i.e.
-                // camera).
-                andyObject.transform.Rotate(0, k_ModelRotation, 0, Space.Self);
-
-                // Create an anchor to allow ARCore to track the hitpoint as understanding of
-                // the physical world evolves.
-                var anchor = hit.Trackable.CreateAnchor(hit.Pose);
-
-                // Make Andy model a child of the anchor.
-                andyObject.transform.parent = anchor.transform;
             }
-        }
+        }        
     }
 
-
-    #region Check and update the application lifecycle.
-    private void _UpdateApplicationLifecycle()
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        // Exit the app when the 'back' button is pressed.
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
-
-        // Only allow the screen to sleep when not tracking.
-        if (Session.Status != SessionStatus.Tracking)
-        {
-            Screen.sleepTimeout = SleepTimeout.SystemSetting;
-        }
-        else
-        {
-            Screen.sleepTimeout = SleepTimeout.NeverSleep;
-        }
-
-        if (m_IsQuitting)
-        {
-            return;
-        }
-
-        // Quit if ARCore was unable to connect and give Unity some time for the toast to
-        // appear.
-        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
-        {
-            _ShowAndroidToastMessage("Camera permission is needed to run this application.");
-            m_IsQuitting = true;
-            Invoke("_DoQuit", 0.5f);
-        }
-        else if (Session.Status.IsError())
-        {
-            _ShowAndroidToastMessage(
-                "ARCore encountered a problem connecting.  Please start the app again.");
-            m_IsQuitting = true;
-            Invoke("_DoQuit", 0.5f);
-        }
+        //throw new System.NotImplementedException();
     }
-    #endregion
-
-
-    #region Actually quit the application.
-    private void _DoQuit()
-    {
-        Application.Quit();
-    }
-
-    /// <summary>
-    /// Show an Android toast message.
-    /// </summary>
-    /// <param name="message">Message string to show in the toast.</param>
-    private void _ShowAndroidToastMessage(string message)
-    {
-        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-        AndroidJavaObject unityActivity =
-            unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-
-        if (unityActivity != null)
-        {
-            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
-            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
-            {
-                AndroidJavaObject toastObject =
-                    toastClass.CallStatic<AndroidJavaObject>(
-                        "makeText", unityActivity, message, 0);
-                toastObject.Call("show");
-            }));
-        }
-    }
-    #endregion
 }

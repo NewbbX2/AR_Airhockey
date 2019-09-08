f76dc33f6da1f09d0af4a67ae575bbc5ad6ad930
YHK416 <42706923+YHK416@users.noreply.github.com>
2019-09-08 22:38:55 +0900

    Update PlayerCharacter.cs

--- Player/PlayerCharacter.cs
+++ Player/PlayerCharacter.cs
@@ -5,15 +5,25 @@ using UnityEngine.UI;
 public class PlayerCharacter : MonoBehaviour
 {
 
+    //자동처리되는 스크립트.
+
+
+    //이것은 오브젝트에 달아놓는게 아니라
+    //생성되어 자동으로 달림.
+    //따라서 따로 관리할필요없다.
+
+
+
+    //플레이어번호. 이전에 지정해주었을것이다.
     public int playerNo = 0;
-    public GameObject StickObject;
-    public Text ScoreText;
 
-
-
+    //점수관리때문에 게임다시 가져옴.
     Game gameOBJ;
+    //보드위치 확인용.
     GameObject hockeyBoard;
 
+    //해당 플레이어의 Puck
+    public GameObject StickObject;
 
 
     void Start()
@@ -41,21 +51,21 @@ public class PlayerCharacter : MonoBehaviour
     //하키스틱 찾은뒤 배치.
     public void PuckStartSetting()
     {
-        StickObject = GameObject.FindGameObjectsWithTag("Puck")[playerNo-1];
-        StickObject.name = "HockeyPuck";
-        StickObject.transform.localScale = new Vector3(1f, 0.5f, 0.1f);
-        StickObject.AddComponent<HockeyStick>();
-        StickObject.GetComponent<HockeyStick>().HockeyStickUserNo = playerNo;
+        StickObject = GameObject.FindGameObjectsWithTag("Puck")[playerNo - 1];
+        StickObject.name = "HockeyPuck" + playerNo;
+        if (playerNo == 1)
+        {
+            StickObject.transform.localScale = new Vector3(1f, 0.5f, 0.1f);
+        }
+        else if(playerNo==2)
+        {
+            StickObject.transform.localScale = new Vector3(1f, 0.5f, 0.1f);
+        }
 
-    }
-
-
-
-    //스코어 
-    public void SetScoreText()
-    {
-        ScoreText.text = "Team1:" + gameOBJ.Score[0] + "Team2:" + gameOBJ.Score[1];
-    }
+        StickObject.AddComponent<HockeyStickInf>();
+        StickObject.GetComponent<HockeyStickInf>().HockeyStickUserNo = playerNo;
+        StickObject.GetComponent<HockeyStickInf>().HockeyStickUserNo = playerNo;
+        }
 
 
 

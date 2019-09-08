15c37bfc61da3895a0193ebe967acc64d8f3f58f
YHK416 <42706923+YHK416@users.noreply.github.com>
2019-09-08 22:02:42 +0900

    Update Game.cs

--- Player/Game.cs
+++ Player/Game.cs
@@ -6,33 +6,34 @@ public class Game : MonoBehaviour
 {
 
 
+    //시작전 세팅
+    //EmptyObject의 name을 GameOBJ로 수정한뒤, 이 스크립트를 달아준다.
 
 
-    //Ball, PlayerCharacter를 관리할거임.
-    //PlayerCharacter는 Stick 관리할거임.
 
 
+    /*
+    //이 스크립트의 역할
+       //Ball과 Playercharacter를 관리할 것이다.
+           //Playercharacter는 스틱을 관리할 것이다.
+       //Ball은 종합적인 공의 동작을 관리한다.
+    //Score를 관리한다.
+         //정확히는 저장하는 것이며 Palyer에서 이 저장된 점수를 표기할것이다.
+    */
+    
+
+
+    //공
     public GameObject BallObject;
+    //점수
     public int[] Score = new int[2];
-    public GameObject[] goal = new GameObject[2];
-
 
     void Start()
     {
+        //플레이어 생성
         CreatePlayer(1); CreatePlayer(2);
+        //첫 골을 스폰.
         SpawnNewBall(new Vector3(0, 1F, 0), new Vector3(0, 0, 10));
-        for (int i = 0; i <= 1; i++)
-        {
-            Score[i] = 0;
-            if (GameObject.FindGameObjectsWithTag("Goal")[0].GetComponent<GoalInf>().TeamNo == i)
-            {
-                goal[i] = GameObject.FindGameObjectsWithTag("Goal")[0];
-            }
-            else if (GameObject.FindGameObjectsWithTag("Goal")[1].GetComponent<GoalInf>().TeamNo == i)
-            {
-                goal[i] = GameObject.FindGameObjectsWithTag("Goal")[1];
-            }
-        }
     }
 
 
@@ -41,21 +42,29 @@ public class Game : MonoBehaviour
     {
         GameObject player = new GameObject();
         player.AddComponent<PlayerCharacter>();
+        player.name = "player" + n;
         player.GetComponent<PlayerCharacter>().playerNo = n;
     }
 
 
-    //일단은 스피어 만드는식인데
+    //일단은 스피어(납작) 만드는식인데
     //실제 디자인 끝나면 주석으로 대체
     public void SpawnNewBall(Vector3 spawnPosition, Vector3 ballDirection)
     {
+        //볼을 스폰하는 방식.(볼 제작 완료시 아래방식 사용)
         BallObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        //BallObject=GameObject.Find("Ball");
-        BallObject.name = "HockeyBall";
-        BallObject.tag = "Ball";
         BallObject.transform.position = spawnPosition;
         BallObject.transform.localScale = new Vector3(0.5f, 0.1f, 0.5f);
         BallObject.AddComponent<Ball>();
+        BallObject.name = "HockeyBall";
+        BallObject.tag = "Ball";
+
+
+        //볼을 복제하는 방식.
+        BallObject = Instantiate(GameObject.Find("Ball"), spawnPosition, Quaternion.identity);
+
+
+        //공통, 방향지정해서 움직이기.
         BallObject.GetComponent<Ball>().BallMoveMent = ballDirection;
     }
 }

841b2f02e47f2bc12365d6839f352a344d89cbbe
DoTroll5610 <51203139+DoTroll5610@users.noreply.github.com>
2019-09-26 01:37:32 +0900

    corner에서 퍽이 반사되는 효과 추가

--- Player/Puck.cs
+++ Player/Puck.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-///  Puck은 종합적인 공의 동작을 관리합니다.
-/// 줄 36과 45을 주석처리함. 이는 생성한 벽이 좌우앞뒤 구분이 없기 때문이고, addforce를 퍽에 매번 적용시키는 것보다 마찰을
-/// 0으로 만드는게 당장 에디터에서 할땐 나아서. 만약 마찰을 넣으려면 addforce값과의 균형을 맞출것 
-/// Puck의 Collider에서 Material의 Friction(마찰) 값을 0.0001로 설정했음
+/// Puck은 종합적인 공의 동작을 관리합니다.
+/// 마찰을 넣으려면 addforce값과의 균형을 맞출것 
+/// Puck의 Collider에서 Material의 Friction(마찰) 값을 0.0001로 설정했음. puck의 mass는 0.1로 설정
 /// </summary>
 public class Puck : MonoBehaviour
 {
@@ -32,23 +31,22 @@ public class Puck : MonoBehaviour
 
     //매번 볼동작시킴.
     void Update()
-    {        
-        //Rigidbody에 힘을 작용시켜서 볼을 동작시킴.
-        //_Rigidbody.AddForce(Movement);
-
+    {
+        Movement = _Rigidbody.velocity; //에디터 상에서 움직임 확인 위해서, 이제 움직임을 주는데 쓰지 않음
     }
 
     //충돌한 오브젝트에 따라 동작.
     void OnCollisionEnter(Collision coll)
     {
         GameObject hitObject = coll.gameObject;
-        Vector3 vec3 = Movement;
-        /* 단일벽이라서 주석처리
+        Vector3 vec3;
+        
         if (hitObject.name == "Wall_Left" || hitObject.name == "Wall_Right")
         {
-            Movement *= Elasticity;
-            Movement.x *= -1;
+            Vector3 CurrentVelocity = _Rigidbody.velocity;
+            CurrentVelocity.x *= -1;
         }
+        /* 
         else if (hitObject.name == "Wall_Front" || hitObject.name == "Wall_Back")
         {
             Movement *= Elasticity;
@@ -58,13 +56,13 @@ public class Puck : MonoBehaviour
         if (hitObject.tag == "Striker")
         {
             HockeyStriker hockeyStickInfor = hitObject.GetComponent<HockeyStriker>();
-            Movement = hockeyStickInfor.StickMoveBall() * 100000f; //이정도 값을 해야 좀 속도가 났음
-            _Rigidbody.AddForce(Movement);
+            vec3 = hockeyStickInfor.StickMoveBall() * 10f; //이정도 값을 해야 좀 속도가 났음,
+            _Rigidbody.AddForce(vec3);
         }
     }
 
 
-    //골에 닿을시 작동.
+    //골에 닿을시 작동. 코너 트리거 박스면 튕기는 듯한 이펙트
     private void OnTriggerEnter(Collider trigger)
     {
         /*
@@ -88,6 +86,12 @@ public class Puck : MonoBehaviour
 
         }
         */
+        if (trigger.tag == "Corner")
+        {
+            //반사각 계산을 위한 코너 벽면의 법선 산출
+            Vector3 inNormal_OfTrigger = new Vector3(Mathf.Cos(Mathf.Deg2Rad * trigger.transform.eulerAngles.y), 0, -Mathf.Sin(Mathf.Deg2Rad * trigger.transform.eulerAngles.y));
+            _Rigidbody.velocity = Vector3.Reflect(_Rigidbody.velocity, inNormal_OfTrigger);//물체가 반사되는 듯한 효과
+        }
         switch (trigger.tag)
         {
             case "Team1":
@@ -98,7 +102,7 @@ public class Puck : MonoBehaviour
             case "Team2":
                 _GameController.ScoreUp(1);
                 _GameController.SpawnNewPuck(2);
-                break;                
+                break;
         }
     }
 

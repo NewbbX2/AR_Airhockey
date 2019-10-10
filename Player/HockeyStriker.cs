b7de52eb119e9da82210c5baa3e0dd751aa585f1
YHK416 <42706923+YHK416@users.noreply.github.com>
2019-10-11 07:34:28 +0900

    Update HockeyStriker.cs

--- Player/HockeyStriker.cs
+++ Player/HockeyStriker.cs
@@ -1,150 +1,149 @@
+#define MOUSE
 using System.Collections;
 using System.Collections.Generic;
-using System;
 using UnityEngine;
-using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class HockeyStriker : MonoBehaviour
+/// <summary>
+/// 하키 Striker를 조작하기 위한 클래스
+/// </summary>
+
+enum PlayerNumber
 {
-    //유저번호
-    [Range(1,2)]public int UserNo = 1;
-    //스틱 이동방향
-    Vector3 movementVectorToAffectBall = new Vector3(0, 0, 0);
+    Player1,
+    Player2,
+}
+public class MoveStriker : MonoBehaviourPunCallbacks, IPunObservable
+{
+    #region 공개 변수들
+    public float PokeForce = 5.0f;//찌르는 듯한 물리효과의 강도
+    public float MoveSpeed = 20.0f; // 이동속도
+    public GameObject MiddlePoint; //경기장 중앙 지점
+    [Range(0, 1)] public int Controller;
+    #endregion
 
-    //보드 오브젝트
-    GameObject hockeyBoard;
+    #region 내부 변수
 
+    private bool isTouched = false;
+
+
+    private Vector2 TouchPos;//터치된 위치
+    private Vector3 TouchVector;//Ray쏠 방향
+    private RaycastHit RayHit;
+    private GameObject HockeyBoard;
+    private Vector3 StickDestination;
+    private float MaxZ;
+    private float MiddlePointZ; //경기장 중앙 지점의 Z좌표값
     private Rigidbody StrikerRigidbody;
+    private PlayerNumber PlayerNum;
+    #endregion
 
 
-    //시작세팅
+
     void Start()
     {
-        hockeyBoard = GameObject.FindGameObjectWithTag("Table");
         StrikerRigidbody = GetComponent<Rigidbody>();
-        //네트워크에서는 필요없음
-        /*
-        //Striker 태그와 배열 index로 특정짓기
-        HockeyStickObjectSelf = GameObject.FindGameObjectsWithTag("Striker")[HockeyStickUserNo - 1];
-        */
+        MiddlePointZ = MiddlePoint.transform.position.z;
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PlayerNum = PlayerNumber.Player1;
+        }
+        else
+        {
+            PlayerNum = PlayerNumber.Player2;
+        }
+        if ((int)PlayerNum == Controller && !photonView.IsMine)
+        {
+            photonView.RequestOwnership();
+        }
     }
 
-
-
-
-    public Vector3 StickMoveBall()
-    {
-        return movementVectorToAffectBall;
-    }
-
-    DateTime T_Now;
-    DateTime T_Past;
-    bool checkisStickMovedNow = false;
     void Update()
     {
-
-        T_Past = T_Now;
-        T_Now = DateTime.Now;
-        Loc_Past = Loc_Now;
-        Loc_Now = transform.position;
-
-
-        if (Loc_Now == Loc_Past)
+        if (!photonView.IsMine)
         {
-            checkisStickMovedNow = false;
-        }
-        else
-        {
-            checkisStickMovedNow = true;
-        }
-
-        //움직였을경우 현재속도 측정.
-        if (checkisStickMovedNow)
-        {
-            TimeSpan TS = T_Now - T_Past;
-            //movementVectorToAffectBall = (Loc_Now - Loc_Past) / TS.Seconds;
-            movementVectorToAffectBall = StrikerRigidbody.velocity;
-        }
-        //현재 이동량이 0이거나
-        //이전에 이동했던게 너무 약할시
-        else
-        {
-            //너무 약해진경우 벽처럼
-            if (movementVectorToAffectBall.sqrMagnitude <= 0.1)
+            if ((transform.position - currentPos).sqrMagnitude <= 10.0f * 10.0f)
             {
-                //calculateSitckSpeedAndDirecWithNoMove();
+                transform.position = currentPos;
             }
-            //아직 덜약해진경우 동작스틱처럼(단, 시간당 파워감소)
             else
             {
-                movementVectorToAffectBall *= 0.99f;
+                transform.position = Vector3.Lerp(transform.position, currentPos,  10.0f);
             }
+            return;
         }
-
+#if UNITY_EDITOR || MOUSE
+        if (!Input.GetMouseButton(0) || (int)PlayerNum != Controller)
+        {
+            return;
+        }
+#else
+        if (Input.touchCount == 0)
+        {
+            return;
+        }
+#endif
+        FindTouchPosition(); //터치 포지션 특정
+        StartCoroutine(TouchStick());//하키 채 움직이기
     }
 
-
-    Vector3 Loc_Now = new Vector3(0, 0, 0);
-    Vector3 Loc_Past = new Vector3(0, 0, 0);
-
-
-    void calculateSitckSpeedAndDirecWithNoMove()
+    private void FindTouchPosition() //Raycast로 하키 채가 움직일 위치 찾기
     {
-        //스틱 조작후 속도 빛 방향=
-        //일단 고정된 스틱인경우의 공이 부딫칠때, 공의 이동방향 및 속도.
-        movementVectorToAffectBall = GameObject.Find("Puck").GetComponent<Puck>().Movement;
-        movementVectorToAffectBall.z *= -1;
+#if UNITY_EDITOR || MOUSE
+        TouchPos = Input.mousePosition;
+#else
+        TouchPos = Input.GetTouch(0).position;
+#endif
+        TouchVector = new Vector3(TouchPos.x, TouchPos.y, 0.0f);
+        Ray TouchRay = Camera.main.ScreenPointToRay(TouchVector);//터치한 방향으로 Ray 설정
 
+        if (Physics.Raycast(TouchRay, out RayHit, Mathf.Infinity))
+        {
+            if (RayHit.collider.tag == "Table")
+            {
+
+                MaxZ = RayHit.point.z;
+                 
+                if ( (PlayerNum == PlayerNumber.Player1) && MaxZ >= MiddlePointZ) //하키가 중앙선 못넘게
+                {
+                    MaxZ = MiddlePointZ;
+                } 
+                else if((PlayerNum == PlayerNumber.Player2) && MaxZ <= MiddlePointZ)
+                {
+                    MaxZ = MiddlePointZ;
+                }
+
+                StickDestination = new Vector3(RayHit.point.x, RayHit.point.y + 0.5f, MaxZ);//테이블 바닥에 닿으면 위치 정보 저장
+            }
+            else
+            {
+            }
+        }
     }
-    //스틱 이동불가 지역 판정
-    void stickCannotMoveThere()
+
+    private IEnumerator TouchStick() //하키 채 움직이기
     {
-        //짝수팀(아래)가
-        if (UserNo % 2 == 0)
-        {
-            //스틱위치:테이블 절반이상일시.
-            if (transform.position.z > 0)
-            {
-                transform.position = new Vector3(transform.position.x, transform.position.y, 0);
-
-            }
-            //스틱위치:테이블 아래일시
-            //스틱위치:테이블위일시.
-            else if (transform.position.z < hockeyBoard.transform.localScale.z)
-            {
-                transform.position = new Vector3(transform.position.x, transform.position.y, -hockeyBoard.transform.localScale.z / 2);
-            }
-
-        }
-        //홀수팀(위)가 
-        else if (UserNo % 2 == 1)
-        {
-            //스틱위치:테이블 절반아래일떄
-            if (transform.position.z < 0)
-            {
-                transform.position = new Vector3(transform.position.x, transform.position.y, 0);
-            }
-            //스틱위치:테이블위일시.
-            else if (transform.position.z > hockeyBoard.transform.localScale.z)
-            {
-                transform.position = new Vector3(transform.position.x, transform.position.y, hockeyBoard.transform.localScale.z / 2);
-            }
-        }
-        //좌우로 벗어난 경우, 테이블 끝으로 가져옴.
-        //벽이 어떤식인지 몰라서 일단 좌우로 나눔.
-        if (transform.position.x > hockeyBoard.transform.localScale.x / 2)
-        {
-            transform.position = new Vector3(hockeyBoard.transform.localScale.x / 2, transform.position.y, transform.position.z);
-        }
-        else if (transform.position.x < -hockeyBoard.transform.localScale.x / 2)
-        {
-            transform.position = new Vector3(hockeyBoard.transform.localScale.x / 2, transform.position.y, transform.position.z);
-        }
+        StrikerRigidbody.velocity = (StickDestination - transform.position).normalized*10.0f;
+        /*인전 움직이는 부분
+        transform.position = Vector3.MoveTowards(transform.position, StickDestination, MoveSpeed * Time.deltaTime);
+        _Rigidbody.AddForce(transform.position - StickDestination);
+        */
+        yield return new WaitForSeconds(0.1f); //0.1초마다 호출
     }
 
-
-
-
-
-
+    #region 포톤뷰 통신부분
+    private Vector3 currentPos;
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+        }
+        else
+        {
+            currentPos = (Vector3)stream.ReceiveNext();
+        }
+    }
+    #endregion
 }
